b5175f9017b35afeaa30431234a0b050b7968323
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-09-24 02:54:46 -0300

    Auth users

--- src/app/controllers/SignInController.ts
+++ src/app/controllers/SignInController.ts
@@ -1,10 +1,43 @@
 import { Request, Response } from 'express'
 import { ValidationHelper } from '../helpers'
 
+import { UserEntity } from '../entities'
+import { getRepository } from 'typeorm'
+
+import bcrypt from 'bcryptjs'
+
+import jwt from 'jsonwebtoken'
+
 export default class SignInController {
   static async store(request: Request, response: Response) {
     try {
       await ValidationHelper.hasErrors(request)
+
+      const { email, password } = request.body
+
+      const user = await getRepository(UserEntity).findOne({ where: { email } })
+      console.log(user)
+      if (!user)
+        return response.status(400).json({
+          message: 'Email ou senha incorretoss'
+        })
+
+      const compareHash = await bcrypt.compare(password, user.password)
+
+      if (compareHash) {
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: parseInt(process.env.JWT_EXPIRES)
+        })
+
+        return response.status(200).json({
+          token,
+          expiresIn: process.env.JWT_EXPIRES
+        })
+      }
+
+      return response.status(400).json({
+        message: 'Email ou senha incorretos'
+      })
     } catch (error) {
       return response.status(400).json(error)
     }
--- src/app/controllers/SignUpController.ts
+++ src/app/controllers/SignUpController.ts
@@ -2,12 +2,21 @@ import { Request, Response } from 'express'
 import { ValidationHelper } from '../helpers'
 
 import { UserEntity } from '../entities'
-
 import { getRepository } from 'typeorm'
 export default class SignUpController {
   static async store(request: Request, response: Response) {
     try {
       await ValidationHelper.hasErrors(request)
+
+      const { email } = request.body
+
+      const checkUserAlreadyExists = await getRepository(UserEntity).findOne({
+        where: { email }
+      })
+      if (checkUserAlreadyExists)
+        return response
+          .status(400)
+          .json({ message: 'Já existe um usuário com esse email' })
 
       const user = await getRepository(UserEntity).create(request.body)
       const results = await getRepository(UserEntity).save(user)
